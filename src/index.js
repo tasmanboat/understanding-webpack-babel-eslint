75f0d602df83c41c154730397d3ea5148de9e1dd
noname <noname@nono.com>
2020-11-09 19:32:15 +0800

    update

--- src/index.js
+++ src/index.js
@@ -13,6 +13,22 @@ function component(){
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack', n], ' ');
   element.classList.add('hello');
+
+  // --- ES6, which is supported by default(without "jsx supported by babel" thing) by webpack ---
+  const getMsg = () => "cappuccino for now, please";
+  let getResult = (x, y) => x + y;
+  getResult = (x, y) => x * y;
+  getResult = (x, y) => { x * y; return 380; };
+  element.innerHTML = getMsg();
+  element.innerHTML = getResult(2,9);
+  const materials = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+  ];
+  const arr1 = materials.map(material => material.length);
+  element.innerHTML = arr1.length;
   return element;
 }
 document.body.appendChild(component());
