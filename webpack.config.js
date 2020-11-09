e1cac1dbc76777dc573b8c3c9e2387112beeb31f
noname <noname@nono.com>
2020-11-09 20:03:48 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
       },
       {
         test: /\.(js|jsx)$/,
-        exclude: /node_modules|bower_components/,
+        exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
           options: {
